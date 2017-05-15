25992bc8782756a617a716d09c47e00fd8e3d623
hoinarut <tudor.hoinaru@gmail.com>
2017-05-15 10:04:59 +0300

    changes

--- getting_started/sample_enum.exs
+++ getting_started/sample_enum.exs
@@ -1,4 +1,14 @@
-defmodule Sample.Enum do
-    def first(list) when length(list) == 0, do: nil
-    def first([head | _]), do: head    
+defmodule Sample.Enum do    
+    def first(list, val \\ nil)
+    def first([head | _], _), do: head    
+    def first([], val), do: val
+
+    def add(list, val \\ 0) do
+        trace(val)
+        [val | list]
+    end
+
+    defp trace(string) do
+        IO.puts("The value passed in was #{string}")
+    end
 end
